e974b089357623f51ed7802d2be2944ccea48c7b
Nukul Bhasin <me@nukulb.com>
2014-03-16 13:33:03 -0400

    Adds client code to upload files to S3
    Adds code using angularFileUpload and xml2json to upload a file to AWS
    and parse the response. Adds client tests for the upload and parse as
    well.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,8 +1,56 @@
 'use strict';
 
 angular.module('s3UploadApp')
-  .controller('MainCtrl', function ($scope, $http) {
-    $http.get('/api/awesomeThings').success(function(awesomeThings) {
-      $scope.awesomeThings = awesomeThings;
+    .controller('MainCtrl', function ($scope, $http, $location, $upload) {
+        $scope.imageUploads = [];
+        $scope.abort = function(index) {
+            $scope.upload[index].abort();
+            $scope.upload[index] = null;
+        };
+
+        $scope.onFileSelect = function ($files) {
+            $scope.files = $files;
+            $scope.upload = [];
+            for (var i = 0; i < $files.length; i++) {
+                var file = $files[i];
+                file.progress = parseInt(0);
+                (function (file, i) {
+                    $http.get('/api/s3Policy?mimeType='+ file.type).success(function(response) {
+                        var s3Params = response;
+                        $scope.upload[i] = $upload.upload({
+                            url: 'https://mybucket-dev.s3.amazonaws.com/',
+                                method: 'POST',
+                            data: {
+                                'key' : 's3UploadExample/'+ Math.round(Math.random()*10000) + '$$' + file.name,
+                                'acl' : 'public-read',
+                                'Content-Type' : file.type,
+                                'AWSAccessKeyId': s3Params.AWSAccessKeyId,
+                                'success_action_status' : '201',
+                                'Policy' : s3Params.s3Policy,
+                                'Signature' : s3Params.s3Signature
+                            },
+                            file: file,
+                        }).then(function(response) {
+                            file.progress = parseInt(100);
+                            if (response.status === 201) {
+                                var data = xml2json.parser(response.data),
+                                parsedData;
+                                parsedData = {
+                                    location: data.postresponse.location,
+                                    bucket: data.postresponse.bucket,
+                                    key: data.postresponse.key,
+                                    etag: data.postresponse.etag
+                                };
+                                $scope.imageUploads.push(parsedData);
+
+                            } else {
+                                console.log('Upload Failed');
+                            }
+                        }, null, function(evt) {
+                            file.progress =  parseInt(100.0 * evt.loaded / evt.total);
+                        });
+                    });
+                }(file, i));
+            }
+        };
     });
-  });
--- test/client/spec/controllers/main.js
+++ test/client/spec/controllers/main.js
@@ -2,27 +2,163 @@
 
 describe('Controller: MainCtrl', function () {
 
-  // load the controller's module
-  beforeEach(module('s3UploadApp'));
+    // load the controller's module
+    beforeEach(module('s3UploadApp'));
+    var $httpBackend,
+        $rootScope,
+        $location,
+        createController;
 
-  var MainCtrl,
-    scope,
-    $httpBackend;
+    // Initialize the controller and a mock scope
+    beforeEach(inject(function($injector) {
+        $httpBackend = $injector.get('$httpBackend');
 
-  // Initialize the controller and a mock scope
-  beforeEach(inject(function (_$httpBackend_, $controller, $rootScope) {
-    $httpBackend = _$httpBackend_;
-    $httpBackend.expectGET('/api/awesomeThings')
-      .respond(['HTML5 Boilerplate', 'AngularJS', 'Karma', 'Express']);
-    scope = $rootScope.$new();
-    MainCtrl = $controller('MainCtrl', {
-      $scope: scope
+        //TODO: Add back when auth is working
+        //$httpBackend.when('GET', '/auth.py').respond({userId: 'userX'}, {'A-Token': 'xxx'});
+        $rootScope = $injector.get('$rootScope');
+        $location = $injector.get('$location');
+        var $controller = $injector.get('$controller');
+        createController = function() {
+            return $controller('MainCtrl', {'$scope' : $rootScope });
+        };
+
+    }));
+
+    afterEach(function() {
+        $httpBackend.verifyNoOutstandingExpectation();
+        $httpBackend.verifyNoOutstandingRequest();
     });
-  }));
 
-  it('should attach a list of awesomeThings to the scope', function () {
-    expect(scope.awesomeThings).toBeUndefined();
-    $httpBackend.flush();
-    expect(scope.awesomeThings.length).toBe(4);
-  });
+    it('should get Policy for one file', function () {
+        var files = [
+            {
+                type: 'image/jpeg',
+                name: 'abc.jpeg'
+            }
+        ];
+
+        createController();
+
+        $httpBackend.expectGET('/api/s3Policy?mimeType='+ files[0].type)
+            .respond({
+                s3Policy: 'base64Policy',
+                s3Signature: 'signature',
+                AWSAccessKeyId: 'config.accessKeyId'
+            });
+
+        $rootScope.onFileSelect(files);
+
+        $httpBackend.expectPOST('https://mybucket-dev.s3.amazonaws.com/')
+            .respond(201,
+            '<?xml version="1.0" encoding="UTF-8"?>'+
+            '<PostResponse>'+
+                '<Location>https://mybucket-dev.s3.amazonaws.com/clubs%2Fimages%2Fpic+copy+3.jpg</Location>'+
+                '<Bucket>mybucket-dev</Bucket>'+
+                '<Key>clubs/images/pic copy 3.jpg</Key>'+
+                '<ETag>"971d696d8ef20fbe43387c817f83dc52"</ETag>'+
+            '</PostResponse>'
+        );
+        $httpBackend.flush();
+    });
+
+    it('should get Policy for multiple files', function () {
+        var files = [
+            {
+                type: 'image/jpeg',
+                name: 'abc.jpeg'
+            },
+            {
+                type: 'image/bmp',
+                name: 'xyz.bmp'
+            }
+        ];
+
+        createController();
+        files.forEach(function (file) {
+            $httpBackend.expectGET('/api/s3Policy?mimeType='+ file.type)
+                .respond({
+                    s3Policy: 'base64Policy',
+                    s3Signature: 'signature',
+                    AWSAccessKeyId: 'config.accessKeyId'
+                });
+        });
+
+        $rootScope.onFileSelect(files);
+
+        files.forEach(function (file) {
+            $httpBackend.expectPOST('https://mybucket-dev.s3.amazonaws.com/')
+                .respond(201,
+                    '<?xml version="1.0" encoding="UTF-8"?>'+
+                    '<PostResponse>'+
+                        '<Location>https://mybucket-dev.s3.amazonaws.com/clubs%2Fimages%2Fpic+copy+3.jpg</Location>'+
+                        '<Bucket>mybucket-dev</Bucket>'+
+                        '<Key>clubs/images/' + file.name + '</Key>'+
+                        '<ETag>"971d696d8ef20fbe43387c817f83dc52"</ETag>'+
+                    '</PostResponse>');
+        });
+
+        $httpBackend.flush();
+    });
+
+    describe('Progress and Abort', function () {
+        beforeEach(function () {
+            var files = [
+                {
+                    type: 'image/jpeg',
+                    name: 'abc.jpeg'
+                },
+                {
+                    type: 'image/bmp',
+                    name: 'xyz.bmp'
+                }
+            ];
+
+            createController();
+            files.forEach(function (file) {
+                $httpBackend.expectGET('/api/s3Policy?mimeType='+ file.type)
+                    .respond({
+                        s3Policy: 'base64Policy',
+                        s3Signature: 'signature',
+                        AWSAccessKeyId: 'config.accessKeyId'
+                    });
+            });
+
+            $rootScope.onFileSelect(files);
+
+            files.forEach(function (file) {
+                $httpBackend.expectPOST('https://mybucket-dev.s3.amazonaws.com/')
+                    .respond(201,
+                        '<?xml version="1.0" encoding="UTF-8"?>'+
+                        '<PostResponse>'+
+                            '<Location>https://mybucket-dev.s3.amazonaws.com/clubs%2Fimages%2Fpic+copy+3.jpg</Location>'+
+                            '<Bucket>mybucket-dev</Bucket>'+
+                            '<Key>clubs/images/' + file.name + '</Key>'+
+                            '<ETag>"971d696d8ef20fbe43387c817f83dc52"</ETag>'+
+                        '</PostResponse>');
+            });
+
+        });
+        it('should update the progress bar', function () {
+
+            expect($rootScope.files[0].progress).toBe(0);
+            expect($rootScope.files[1].progress).toBe(0);
+
+            $httpBackend.flush();
+
+            expect($rootScope.files[0].progress).toBe(100);
+            expect($rootScope.files[1].progress).toBe(100);
+        });
+
+        it('should allow aborting existing downloads', function () {
+            expect($rootScope.abort).toBeDefined();
+
+            $httpBackend.flush();
+
+            expect($rootScope.upload.length).toBe(2);
+            expect($rootScope.upload[0].abort).toBeDefined();
+            expect($rootScope.upload[1].abort).toBeDefined();
+        });
+
+    });
+
 });
