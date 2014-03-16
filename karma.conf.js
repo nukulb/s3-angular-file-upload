12f4bb85c45b686a8f6d23961902fbf732f3b9de
Nukul Bhasin <me@nukulb.com>
2014-03-16 13:30:54 -0400

    Adds xml2json as a new dependency.
    Adds a new module under the thirdParty folder since its not available
    using bower. This will be used to parse the response from AWS when a new
    file uploaded.

--- karma.conf.js
+++ karma.conf.js
@@ -17,6 +17,7 @@ module.exports = function(config) {
       'app/bower_components/angular-sanitize/angular-sanitize.js',
       'app/bower_components/angular-route/angular-route.js',
       'app/bower_components/ng-file-upload/angular-file-upload.js',
+      'app/thirdParty/xml2json/xml2json.js',
       'app/scripts/*.js',
       'app/scripts/**/*.js',
       'test/client/spec/**/*.js'
