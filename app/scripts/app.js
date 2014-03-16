da5c8cfc9cae567700d44cbd4e5af6d40dd3f907
Nukul Bhasin <me@nukulb.com>
2014-03-16 08:23:20 -0400

    Adds a basic angular and express app using yeoman.
    Generates a basic app using yeoman and generator-angular-fullstack. Out
    of the box we have basic angular and express app along with tests for
    both the client and server located under the test folder.

---
+++ app/scripts/app.js
@@ -0,0 +1,19 @@
+'use strict';
+
+angular.module('s3UploadApp', [
+  'ngCookies',
+  'ngSanitize',
+  'ngRoute'
+])
+  .config(function ($routeProvider, $locationProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'partials/main',
+        controller: 'MainCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+      
+    $locationProvider.html5Mode(true);
+  });
